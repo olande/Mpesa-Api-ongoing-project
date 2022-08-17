356c65c3421d399a7b176eac4da3326cffbdb5bc
erick olande <40099591+olande@users.noreply.github.com>
2022-08-18 02:50:47 +0300

    Mpesa api intergration

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+    <title>Laravel world</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+    <body >
+
+       <div class="container">
+        <h3>hello world</h3>
+
+</div>
+    </body>
+</html>
